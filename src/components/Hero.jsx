34de0b1bf151993a223f34f315b8468be926abcc
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-04 01:06:15 +0530

    Hero created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { styles } from '../style';
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+   <section className="relative w-full h-screen mx-auto">
+    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      {/* circle and line  */}
+      <div className="flex flex-col justify-center items-center mt-5">
+        <div className="w-5 h-5 rounded-full bg-[#9153ff]" />
+        <div className="w-1 sm:h-80 h-40 violet-gradient" />
+      </div>
+
+      {/* Intro */}
+      <div className="">
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Abhiraj</span></h1> 
+        <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I develop Fullstack React <br className="sm:block hidden" />web an mobile applications  
+        </p> 
+      </div>  
+
+    </div>
+    <ComputersCanvas  />
+   </section>
   )
 }
 
