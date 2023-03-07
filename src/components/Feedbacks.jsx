45cb9fc3823212c7206e8b06ce3632a07c3bc869
abhi-cyber <abhirajchatrath@gmail.com>
2023-03-07 12:32:53 +0530

    images added

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -20,6 +20,12 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
             </p>
             <p className="mt-1 text-secondary text-[12px]">{designation} of {company}</p>
           </div>
+
+          <img 
+            src={image}
+            alt={`feedback-by-${name}`}
+            className="w-10 h-10 rounded-full object-cover"
+          />
         </div>
       </div>
     </motion.div>
